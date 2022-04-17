773bf7a6a90bbd440e4c1d4ccda93d0542258c2b
Yoichiro Sakurai <saku2saku@gmail.com>
2022-04-17 17:24:36 +0900

    Update files.

--- script/install.sh
+++ script/install.sh
@@ -28,6 +28,10 @@ elif [ $OS == "macOS" ]; then
 fi
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 source "$HOME/.cargo/env"
 
 cargo install cargo-make
+
+# Load cargo setting again
+source "$HOME/.cargo/env"
