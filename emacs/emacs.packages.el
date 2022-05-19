a6e703eb377a2bdf8f3691031dcead9c2f1ffebd
Yoichiro Sakurai <saku2saku@gmail.com>
2022-05-19 12:08:47 +0900

    Add emacs conf files.

---
+++ emacs/emacs.packages.el
@@ -0,0 +1,24 @@
+;;; Usage
+;;; 1. Open this file via emacs
+;;; 2. Input M-x, and exec 'eval-buffer' command
+;;; 3. All favorite packages will be installed.
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(package-refresh-contents)
+
+(defvar favorite-packages
+  '(
+    ;;;; helm
+    helm
+
+    ;;;; ag
+    ag exec-path-from-shell
+    ))
+
+(dolist (package favorite-packages)
+  (unless (package-installed-p package)
+    (package-install package)))
